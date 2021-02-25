ad74834a3dfcafe45e372f6c9bdae0b848b1901f
Electronics_Engineer <sava777cool@gmail.com>
2021-02-25 15:02:41 +0200

    Add blog models attributes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	pub_date = models.DateField(auto_now_add=True)
+	post_image = models.ImageField(upload_to='post_image/')
+	post_text = models.CharField(max_length=1000)
