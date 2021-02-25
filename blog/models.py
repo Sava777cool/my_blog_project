0c634837e86f510281d2ecf4e9738c42db7b87a6
Electronics_Engineer <sava777cool@gmail.com>
2021-02-25 15:15:23 +0200

    Add new date time field to blog

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-	title = models.CharField(max_length=200)
-	pub_date = models.DateField(auto_now_add=True)
+	title = models.CharField(max_length=300)
+	pub_date = models.DateTimeField()
 	post_image = models.ImageField(upload_to='post_image/')
-	post_text = models.CharField(max_length=1000)
+	post_text = models.TextField()
